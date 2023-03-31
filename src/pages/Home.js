7482a1acace6e524d929c0db1815e39f2b598711
mario-website <mario.websie@gmail.com>
2023-03-31 23:20:45 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,12 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <h2>Welcome to the Kayaking Club</h2>
+      <h2>Witaj w Klubie Kajakarskim</h2>
       <p>
-        We are a community of kayaking enthusiasts who love exploring rivers, lakes, and
-        oceans. Join us on our adventures, learn new skills, and make new friends.
-        Everyone is welcome, whether you're a beginner or an experienced paddler.
+        Jesteśmy społecznością entuzjastów kajakarstwa, którzy uwielbiają odkrywać rzeki,
+        jeziora i oceany. Dołącz do nas w naszych przygodach, naucz się nowych
+        umiejętności i zawrzyj nowe znajomości. Zapraszamy wszystkich, zarówno
+        początkujących, jak i doświadczonych wodniaków.
       </p>
     </div>
   );
